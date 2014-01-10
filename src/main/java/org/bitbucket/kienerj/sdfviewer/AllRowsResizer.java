cc999d79c8266208f8e91c6d0762aed628f8bfc2
kienerj <devnull@localhost>
2014-01-10 13:53:57 +0100

    improved all rows resizing but still issue with cursor

--- src/main/java/org/bitbucket/kienerj/sdfviewer/AllRowsResizer.java
+++ src/main/java/org/bitbucket/kienerj/sdfviewer/AllRowsResizer.java
@@ -20,9 +20,13 @@ import java.awt.Cursor;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JViewport;
 import javax.swing.event.MouseInputAdapter;
 import javax.swing.table.JTableHeader;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
 /**
  * Allows a user to resize all rows by dragging the boarder between the table
@@ -32,59 +36,88 @@ import javax.swing.table.JTableHeader;
  */
 public class AllRowsResizer extends MouseInputAdapter {
 
+    private static final XLogger logger = XLoggerFactory.getXLogger("AllRowsResizer");
     public static Cursor resizeCursor = Cursor.getPredefinedCursor(Cursor.N_RESIZE_CURSOR);
     private boolean isResizing = false;
-    private Cursor otherCursor = resizeCursor;
+    private int yOffset;
+    private int topRow;
+    private Cursor tableCursor = resizeCursor;
     private JTable table;
 
     public AllRowsResizer(JTable table) {
         this.table = table;
         table.addMouseListener(this);
         table.addMouseMotionListener(this);
+        table.getTableHeader().addMouseListener(this);
+        table.getTableHeader().addMouseMotionListener(this);
     }
 
-    private boolean isResizingHeader(Point p) {
+    private boolean isResizingHeader(MouseEvent e) {
 
+        Point p = e.getPoint();
+
+        Object source = e.getSource();
         JTableHeader header = table.getTableHeader();
-        if (header.contains(p)) {
 
-            int col = header.columnAtPoint(p);
+        if (source instanceof JTableHeader
+                && header.contains(p)) {
+
+            logger.debug("Mouse in Table header.");
+            logger.debug("Headerheight: {}", header.getHeight());
+            logger.debug("Header Y: {}", header.getY());
+            logger.debug("Point Y: {}", p.y);
+            int col = table.columnAtPoint(p);
             if (col == -1) {
                 return false;
             }
-            Rectangle r = header.getBounds();
-            r.grow(0, -3);
-            return !r.contains(p);
 
-//        } else if (table.rowAtPoint(p) == 0) {
-//
-//            int col = table.columnAtPoint(p);
-//            if (col == -1) {
-//                return false;
-//            }
-//            Rectangle r = table.getCellRect(0, col, true);
-//            r.grow(0, -3);
-//            return !r.contains(p);
-        } else {
-            return false;
+            Boolean result = ((header.getY() + header.getHeight()) - 20) < p.y;
+            logger.debug(result.toString());
+            return result;
+
+        } else if (source instanceof JTable) {
+
+            int topRow = getTopRow();
+            int row = table.rowAtPoint(p);
+
+            if (row == topRow) {
+                logger.debug("Mouse in top row.");
+                int col = table.columnAtPoint(p);
+                if (col == -1) {
+                    return false;
+                }
+                Rectangle r = getCellRectangle(row, col);
+                r.grow(0, -5);
+                return r.y > p.y;
+
+            }
         }
+        return false;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        Point p = e.getPoint();
-        isResizing = isResizingHeader(p);
+        isResizing = isResizingHeader(e);
+        yOffset = e.getYOnScreen();
+        topRow = getTopRow();
+        if (isResizing) {
+            table.setAutoscrolls(false);
+        }
     }
 
-    private void swapCursor() {
-        Cursor tmp = table.getCursor();
-        table.setCursor(otherCursor);
-        otherCursor = tmp;
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (isResizing) {
+            table.setAutoscrolls(true);
+            if (table.getCursor() == resizeCursor) {
+                swapCursor();
+            }
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        if (isResizingHeader(e.getPoint())
+        if (isResizingHeader(e)
                 != (table.getCursor() == resizeCursor)) {
             swapCursor();
         }
@@ -92,13 +125,64 @@ public class AllRowsResizer extends MouseInputAdapter {
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        int mouseY = e.getY();
-
+        int mouseY = e.getYOnScreen();
         if (isResizing) {
-            int newHeight = mouseY + table.getRowHeight();
+            int newHeight = table.getRowHeight() + (mouseY - yOffset);
             if (newHeight > 0) {
+                yOffset = e.getYOnScreen();
                 table.setRowHeight(newHeight);
+                JViewport viewport = (JViewport) table.getParent();
+                JScrollPane scrollPane = (JScrollPane) viewport.getParent();
+                // This rectangle is relative to the table where the
+                // northwest corner of cell (0,0) is always (0,0).
+                Rectangle rect = table.getCellRect(topRow, 0, true);
+                scrollPane.getVerticalScrollBar().setValue(rect.y);
             }
         }
+    }
+
+    private int getTopRow() {
+        JViewport viewport = ((JViewport) table.getParent());
+        Point pViewport = viewport.getViewPosition();
+        return table.rowAtPoint(pViewport);
+    }
+
+    private void swapCursor() {
+        Cursor tmp = table.getCursor();
+        table.setCursor(tableCursor);
+        tableCursor = tmp;
+    }
+
+    /**
+     * Get the rectangle of the visible part of the given table cell. The top
+     * row might only be partially visible.
+     *
+     * @param row
+     * @param col
+     * @return
+     */
+    private Rectangle getCellRectangle(int row, int col) {
+        if (!(table.getParent() instanceof JViewport)) {
+            return table.getCellRect(row, col, true);
+        }
+
+        JViewport viewport = (JViewport) table.getParent();
+        // This rectangle is relative to the table where the
+        // northwest corner of cell (0,0) is always (0,0)
+        Rectangle rect = table.getCellRect(row, col, true);
+        // The location of the viewport relative to the table
+        Point pt = viewport.getViewPosition();
+        // Translate the cell location so that it is relative
+        // to the view, assuming the northwest corner of the
+        // view is (0,0)
+        //rect.setLocation(rect.x - pt.x, rect.y - pt.y);
+        int difference = rect.y - pt.y;
+        if (difference < 0) {
+            //row only partially visiable
+            rect.y = rect.y + difference; // move down
+            rect.height = rect.height - difference; // make smaller
+        }
+        // Check if view completely contains the row
+        return rect;
     }
 }
